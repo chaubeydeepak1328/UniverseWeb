086b701cefeb24ae0eb640f7de3ecfab69ad1e6d
Deepak <chaubeydeepak903@gmail.com>
2025-05-05 19:59:42 +0530

    adding chanes in registration

--- src/components/RightUserPannel1.jsx
+++ src/components/RightUserPannel1.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useStore } from '../Store/UserStore';
 import { useAppKitAccount } from '@reown/appkit/react';
 import { ToastContainer, toast } from 'react-toastify';
+import { FaRegCopy } from "react-icons/fa";
 
 import '../App.css'
 
@@ -57,7 +58,58 @@ const RightUserPannel1 = () => {
     }
 
 
-    const staticReferal = "0x43e76a14e75ae2DE6c8D8799112f11e5E62b767d"
+    const staticReferal = "0x43e76a14e75ae2DE6c8D8799112f11e5E62b767d";
+
+
+
+    const handlePast = async () => {
+        try {
+            const text = await navigator.clipboard.readText();
+            setSponsorAddress(text);
+            toast.success("Address copied to clipboard!", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        } catch (err) {
+            toast.error("Failed to paste from clipboard", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(staticReferal)
+            .then(() =>
+                toast.success("Referral address copied!", {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                }))
+            .catch(() => toast.error("Copy failed", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            }))
+    }
 
 
 
@@ -66,7 +118,7 @@ const RightUserPannel1 = () => {
 
     // className = "w-full px-6 py-8 bg-white rounded-xl shadow-lg max-w-md mx-auto h-fit" 
     return (
-        <div className="grid grid-cols-1 mt-10 border-[#3C71A9] border-2  p-6 text-center w-full px-6 py-8 bg-white rounded-xl shadow-lg max-w-md mx-auto text-white mx-auto h-fit"
+        <div className="grid grid-cols-1 mt-10 border-[#3C71A9] border-2  p-6 text-center w-full px-6 py-8 bg-white rounded-xl shadow-lg max-w-md  text-white mx-auto h-fit"
             style={{
                 background:
                     "linear-gradient(100deg, rgba(5, 53, 102, 1) 0%, rgba(169, 190, 10, 1) 100%)",
@@ -88,61 +140,27 @@ const RightUserPannel1 = () => {
                         className="w-full h-13 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 text-white pr-16 walletAddress"
                     />
 
-                    <button className='absolute right-1 top-1/2 -translate-y-1/2 p-2 border-1 rounded-xl bg-green-400 cursor-pointer' onClick={async () => {
-                        try {
-                            const text = await navigator.clipboard.readText();
-                            setSponsorAddress(text);
-                            toast.success("Address copied to clipboard!", {
-                                position: "top-right",
-                                autoClose: 2000,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                pauseOnHover: true,
-                                draggable: true,
-                                progress: undefined,
-                            });
-                        } catch (err) {
-                            toast.failed("Failed to paste from clipboard", {
-                                position: "top-right",
-                                autoClose: 2000,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                pauseOnHover: true,
-                                draggable: true,
-                                progress: undefined,
-                            });
-                        }
-                    }} >paste</button>
+                    <button className='absolute right-1 top-1/2 -translate-y-1/2 p-2 border-1 rounded-xl bg-green-400 cursor-pointer' onClick={handlePast} >paste</button>
                 </div>
-                <p className="text-sm text-white">{message}</p>
+
+                {message ? <p className={`text-sm p-2 ${(
+                    message === "Valid Sponser address! You can proceed with registration." ||
+                    message === "Registration successful!"
+                ) ? "text-green-400 bg-black" : "text-red-600 bg-black"}`}>
+                    {message}
+                </p> : ""}
 
 
 
-                <label htmlFor="walletAddress" className="block text-sm font-medium text-white">
-                    Dont't have Refferal ? <button className='p-2 bg-warning cursor-pointer' onClick={() => {
-                        navigator.clipboard.writeText(staticReferal)
-                            .then(() =>
-                                toast.success("Referral address copied!", {
-                                    position: "top-right",
-                                    autoClose: 2000,
-                                    hideProgressBar: false,
-                                    closeOnClick: true,
-                                    pauseOnHover: true,
-                                    draggable: true,
-                                    progress: undefined,
-                                }))
-                            .catch(() => toast.failed("Copy failed", {
-                                position: "top-right",
-                                autoClose: 2000,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                pauseOnHover: true,
-                                draggable: true,
-                                progress: undefined,
-                            }));
-                    }}>Copy Referal</button>
-                    <p>{staticReferal.slice(1, 7)} .... {staticReferal.slice(-6)}</p>
-                </label>
+
+                <div className="flex flex-col items-center text-sm font-medium text-white">
+                    <div className='flex flex-row  flex-wrap justify-content-center items-center'>
+                        <p>Don't have Refferal ? </p><button className='p-2 bg-warning cursor-pointer' onClick={handleCopy}>Copy Referral</button>
+                    </div>
+                    <div className='flex flex-row flex-wrap gap-4'>
+                        <p>{staticReferal.slice(1, 7)} .... {staticReferal.slice(-6)}</p> <FaRegCopy className='cursor-pointer' onClick={handleCopy} />
+                    </div>
+                </div>
 
 
 
